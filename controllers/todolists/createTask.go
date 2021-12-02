66708e4cee061097b7076d9594a93781b0f3f5ac
Juan Mira <juanmira20@hotmail.com>
2021-12-02 09:19:58 -0500

    Create a todolist working

--- controllers/todolists/createTask.go
+++ controllers/todolists/createTask.go
@@ -1,11 +1,36 @@
 package todolists
 
 import (
+	"github.com/JuanMira/todolist/models"
+	taskrepositories "github.com/JuanMira/todolist/repositories/taskRepositories"
 	"github.com/gofiber/fiber/v2"
 )
 
 func CreateTask(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"message": "works",
+
+	var todolist *models.ToDoList
+
+	if err := c.BodyParser(&todolist); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(models.Error{
+			Message: "Data sended is incorrect",
+			Err:     err.Error(),
+			Status:  false,
+		})
+	}
+
+	err := taskrepositories.CreateToDoList(todolist)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Error{
+			Message: "Something went wrong while trying create a new todolist, pls try again",
+			Err:     err,
+			Status:  false,
+		})
+	}
+
+	return c.Status(fiber.StatusAccepted).JSON(models.Success{
+		Message: "todolist created successfully",
+		Data:    "",
+		Status:  true,
 	})
 }
